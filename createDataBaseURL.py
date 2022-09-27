d41e673fae4055103dc81ccdb88dfb180f771df0
MitsukiS16 <clarissemtcarvalho@gmail.com>
2022-09-27 17:06:51 +0100

    [A] update data base url

--- createDataBaseURL.py
+++ createDataBaseURL.py
@@ -3,23 +3,28 @@
 #https://www.cardmarket.com/en/YuGiOh/Products/Singles?mode=list&idCategory=5&idExpansion=0&searchString=__&idRarity=0&site=2
 
 
+
+
 def my_createDataBaseURL(): 
 
-  alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  number = ['2','3','4','5','6','7','8','9','10','11','12','13','14','15']
+  with open('urlDB.txt', 'w') as f:
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    number = ['2','3','4','5','6','7','8','9','10','11','12','13','14','15']
 
-  firstPart = "https://www.cardmarket.com/en/YuGiOh/Products/Singles?mode=list&idCategory=5&idExpansion=0&searchString="
-  lastPart = "&idRarity=0&perSite=20"
-  secondPart = "&idRarity=0&site="
+    firstPart = "https://www.cardmarket.com/en/YuGiOh/Products/Singles?mode=list&idCategory=5&idExpansion=0&searchString="
+    lastPart = "&idRarity=0&perSite=20"
+    secondPart = "&idRarity=0&site="
 
-  # ends in 1
-  for i in alphabet:
-    for j in alphabet:
-      print(firstPart + i + j + lastPart)
+    # ends in 1
+    for i in alphabet:
+      for j in alphabet:
+        f.write(firstPart + i + j + lastPart)
+        f.write('\n')
 
-  # other
-  for i in alphabet:
-    for j in alphabet:
-      for num in number:
-        print(firstPart + i + j + secondPart + num)
-
+    # other
+    for i in alphabet:
+      for j in alphabet:
+        for num in number:
+          f.write(firstPart + i + j + secondPart + num)
+          f.write('\n')
+          
--- main.py
+++ main.py
@@ -4,22 +4,18 @@ from createDataBase import my_createDataBase
 
 import time
 
-
-
 start_time = time.time()
 
 
-#my_createDataBaseURL()
 
-with open("urlDBaz.txt") as f:
-    content_list = f.readlines()
-for line in content_list:
-    print(line)
-
-
-
+my_createDataBaseURL()
+#with open("urlDBaz.txt") as f:
+#    content_list = f.readlines()
+#for line in content_list:
+#    print(line)
 my_createDataBase()
 my_duplicateDataBase()
+
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
